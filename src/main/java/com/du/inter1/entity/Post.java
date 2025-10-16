72ecb43acf6f24cd1dcbb9f5b99b23eea1cfedd7
jiwoong-jung <jjw981119@naver.com>
2025-10-16 15:47:21 +0900

    생성자 추가

--- src/main/java/com/du/inter1/entity/Post.java
+++ src/main/java/com/du/inter1/entity/Post.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,4 +20,57 @@ public class Post {
 
     @ManyToOne
     private User author;
+
+    public Post() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Post(String title, String content, User author) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.author = author;
+    }
+
+    // Getter and Setter
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public User getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(User author) {
+//        this.author = author;
+//    }
 }
+
