601ea8226c84e9d103d049fc6094291a46f81d53
jiwoong-jung <jjw981119@naver.com>
2025-10-16 15:23:02 +0900

    first commit

---
+++ src/main/java/com/du/inter1/entity/Post.java
@@ -0,0 +1,22 @@
+package com.du.inter1.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private User author;
+}
